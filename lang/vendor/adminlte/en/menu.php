ea2b000569d2c886075a43346b981533b91b4c31
CristianGS <cristiangsh@gmail.com>
2024-07-08 17:57:03 -0600

    Added 'es' and 'en' language files.

---
+++ lang/vendor/adminlte/en/menu.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'main_navigation' => 'MAIN NAVIGATION',
+    'blog' => 'Blog',
+    'pages' => 'Pages',
+    'account_settings' => 'ACCOUNT SETTINGS',
+    'profile' => 'Profile',
+    'change_password' => 'Change Password',
+    'multilevel' => 'Multi Level',
+    'level_one' => 'Level 1',
+    'level_two' => 'Level 2',
+    'level_three' => 'Level 3',
+    'labels' => 'LABELS',
+    'important' => 'Important',
+    'warning' => 'Warning',
+    'information' => 'Information',
+    'main_menu' => 'MAIN MENU',
+    'examples' => 'Examples',
+    'management' => 'Management',
+    'clients' => 'Clients',
+    'products' => 'Products',
+    'users' => 'Users',
+    'roles' => 'Roles',
+    'permissions' => 'Permissions',
+];
---
+++ lang/vendor/adminlte/es/menu.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'main_navigation' => 'MENU PRINCIPAL',
+    'blog' => 'Blog',
+    'pages' => 'Páginas',
+    'account_settings' => 'AJUSTES DE LA CUENTA',
+    'profile' => 'Perfil',
+    'change_password' => 'Cambiar Contraseña',
+    'multilevel' => 'Multi Nivel',
+    'level_one' => 'Nivel 1',
+    'level_two' => 'Nivel 2',
+    'level_three' => 'Nivel 3',
+    'labels' => 'ETIQUETAS',
+    'important' => 'Importante',
+    'warning' => 'Advertencia',
+    'information' => 'Información',
+    'main_menu' => 'MENU PRINCIPAL',
+    'examples' => 'Ejemplos',
+    'management' => 'Gestión',
+    'clients' => 'Clientes',
+    'products' => 'Productos',
+    'users' => 'Usuarios',
+    'roles' => 'Roles',
+    'permissions' => 'Permisos',
+];
